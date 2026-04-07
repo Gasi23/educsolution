ac7b963551ae2ceadfa563ab2ac2cbe6195e0127
Gabriely Victoria <gabrielyv960@gmail.com>
2026-04-07 09:50:59 -0300

    Adiciona barra de forca da senha

--- js/app.js
+++ js/app.js
@@ -3,6 +3,21 @@ function validarSenha() {
   const confirmarSenha = document.getElementById("confirmarSenha").value;
   const mensagem = document.getElementById("mensagem");
 
+  const senhaInput = document.getElementById("senha");
+const forcaSenha = document.getElementById("forcaSenha");
+
+senhaInput.addEventListener("input", () => {
+  const senha = senhaInput.value;
+
+  if (senha.length < 4) {
+    forcaSenha.textContent = "Senha fraca";
+  } else if (senha.length < 8) {
+    forcaSenha.textContent = "Senha média";
+  } else {
+    forcaSenha.textContent = "Senha forte";
+  }
+});
+
   if (senha.length < 6) {
     mensagem.textContent = "A senha precisa ter pelo menos 6 caracteres.";
     return false;
